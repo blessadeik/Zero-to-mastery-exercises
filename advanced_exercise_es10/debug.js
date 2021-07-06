30db98b69543ee5e77d6a736e76962894a0f1fa1
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2021-07-06 12:34:51 +0100

    debug

---
+++ advanced_exercise_es10/debug.js
@@ -0,0 +1,10 @@
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+    (accumulator, array) =>{ 
+        debugger;
+        return accumulator.concat(array)
+    }, []
+);
+
+//
+const flattened =  [[0, 1], [2, 3], [4, 5]].reduce(
+    (accumulator, array) => accumulator.concat(array), [])
